b86ae468877047af0a5ab30a925621a405f58141
Shahbaz Javeed <sjaveed@gmail.com>
2019-08-21 23:56:46 -0400

    #1 - removed use of Matrix.build in preparation for threading
    added support for passing a block to run to track iterations

--- lib/affinity_propagation/calculator.rb
+++ lib/affinity_propagation/calculator.rb
@@ -20,6 +20,8 @@ module AffinityPropagation
 
       "[#{output}]"
     end
+
+    public :[]=
   end
 
   class Calculator
@@ -33,7 +35,7 @@ module AffinityPropagation
       @data = data
       @lambda = lambda
 
-      raise 'no block provide to calculate similarities within data!' unless block_given?
+      raise 'no block provided to calculate similarities within data!' unless block_given?
 
       @similarities = similarity_matrix(&block)
       reset
@@ -70,26 +72,34 @@ module AffinityPropagation
     end
 
     def run(iterations: 100, stable_iterations: 10)
-      iterate while @total_iterations < iterations && @stable_cluster_iterations < stable_iterations
+      while @total_iterations < iterations && @stable_cluster_iterations < stable_iterations
+        iterate
+
+        yield(@total_iterations, @stable_cluster_iterations) if block_given?
+      end
     end
 
     private
 
     def similarity_matrix(&block)
       similarity_array = []
-      similarities = Matrix.build(@data.size, @data.size) do |row_idx, col_idx|
-        exemplar = @data[row_idx]
-        datum = @data[col_idx]
+      similarities = Matrix.zero(@data.size, @data.size)
 
-        similarity = block.call(datum, exemplar)
-        similarity_array << similarity
-        similarity
+      @data.size.times do |row_idx|
+        @data.size.times do |col_idx|
+          exemplar = @data[row_idx]
+          datum = @data[col_idx]
+
+          similarity = block.call(datum, exemplar)
+          similarity_array << similarity
+
+          similarities[row_idx, col_idx] = similarity
+        end
       end
 
       median_similarity = similarity_array.median
 
-      # Matrices can't be modified once created so we have to use this hack
-      (0...@data.size).each { |idx| similarities.send(:[]=, idx, idx, median_similarity) }
+      (0...@data.size).each { |idx| similarities[idx, idx] = median_similarity }
 
       similarities
     end
@@ -99,49 +109,61 @@ module AffinityPropagation
     end
 
     def responsibility_matrix
-      Matrix.build(@similarities.row_count, @similarities.column_count) do |row_idx, col_idx|
-        exemplar_idx = row_idx
-        datum_idx = col_idx
+      responsibilities = Matrix.zero(@similarities.row_count, @similarities.column_count)
 
-        availability_column = @availabilities.column(col_idx).to_a
-        availability_column.slice!(exemplar_idx)
-        similarity_column = @similarities.column(col_idx).to_a
-        similarity_column.slice!(exemplar_idx)
+      @similarities.row_count.times do |row_idx|
+        @similarities.column_count.times do |col_idx|
+          exemplar_idx = row_idx
+          datum_idx = col_idx
 
-        availability_plus_similarity = []
-        availability_column.zip(similarity_column) { |data| availability_plus_similarity << data.sum }
+          availability_column = @availabilities.column(col_idx).to_a
+          availability_column.slice!(exemplar_idx)
+          similarity_column = @similarities.column(col_idx).to_a
+          similarity_column.slice!(exemplar_idx)
 
-        dampen(@similarities[row_idx, col_idx] - availability_plus_similarity.max, @responsibilities[exemplar_idx, datum_idx])
+          availability_plus_similarity = []
+          availability_column.zip(similarity_column) { |data| availability_plus_similarity << data.sum }
+
+          responsibilities[row_idx, col_idx] = dampen(@similarities[row_idx, col_idx] - availability_plus_similarity.max, @responsibilities[exemplar_idx, datum_idx])
+        end
       end
+
+      responsibilities
     end
 
     def availability_matrix
-      Matrix.build(@responsibilities.row_count, @responsibilities.column_count) do |row_idx, col_idx|
-        exemplar_idx = row_idx
-        datum_idx = col_idx
-        responsibility_column = @responsibilities.row(exemplar_idx).to_a
+      availabilities = Matrix.zero(@responsibilities.row_count, @responsibilities.column_count)
 
-        if exemplar_idx == datum_idx
-          # self-availability
-          responsibility_column.slice!(exemplar_idx)
+      @responsibilities.row_count.times do |row_idx|
+        @responsibilities.column_count.times do |col_idx|
+          exemplar_idx = row_idx
+          datum_idx = col_idx
+          responsibility_column = @responsibilities.row(exemplar_idx).to_a
 
-          dampen(responsibility_column.inject(0) { |sum, item| sum += [0, item].max },@availabilities[exemplar_idx, datum_idx])
-        else
-          self_responsibility = @responsibilities[exemplar_idx, exemplar_idx]
-          if datum_idx > exemplar_idx
-            # Slice out the datum index first since in this case it won't affect the exemplar index
-            responsibility_column.slice!(datum_idx)
+          if exemplar_idx == datum_idx
+            # self-availability
             responsibility_column.slice!(exemplar_idx)
+
+            dampen(responsibility_column.inject(0) { |sum, item| sum += [0, item].max },@availabilities[exemplar_idx, datum_idx])
           else
-            responsibility_column.slice!(exemplar_idx)
-            responsibility_column.slice!(datum_idx)
+            self_responsibility = @responsibilities[exemplar_idx, exemplar_idx]
+            if datum_idx > exemplar_idx
+              # Slice out the datum index first since in this case it won't affect the exemplar index
+              responsibility_column.slice!(datum_idx)
+              responsibility_column.slice!(exemplar_idx)
+            else
+              responsibility_column.slice!(exemplar_idx)
+              responsibility_column.slice!(datum_idx)
+            end
+
+            responsibility_column_sum = responsibility_column.inject(0) { |sum, item| sum += [0, item].max }
+
+            availabilities[row_idx, col_idx] = dampen([0, self_responsibility + responsibility_column_sum].min, @availabilities[exemplar_idx, datum_idx])
           end
-
-          responsibility_column_sum = responsibility_column.inject(0) { |sum, item| sum += [0, item].max }
-
-          dampen([0, self_responsibility + responsibility_column_sum].min, @availabilities[exemplar_idx, datum_idx])
         end
       end
+
+      availabilities
     end
 
     def identify_raw_clusters
